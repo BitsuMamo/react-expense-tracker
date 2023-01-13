b12f9f46fcce0719a33b0590aa015e3dcf1278e6
Bitsu Mamo <bitsua.mamo@gmail.com>
2023-01-13 14:17:34 -0600

    Added components to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import BalanceView from "./components/BalanceView";
+import Header from "./components/Header";
+import IncomeExpenseCard from "./components/IcomeExpenseCard";
+import History from "./components/History";
+import AddTransaction from "./components/AddTransaction";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="container">
+            <Header />
+            <BalanceView />
+            <IncomeExpenseCard />
+
+            <History />
+            <AddTransaction />
+
+        </div>
+    );
 }
 
 export default App;
