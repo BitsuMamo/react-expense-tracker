5789aa7817c98039fd1312bd052ce0168944a5a5
Bitsu Mamo <bitsua.mamo@gmail.com>
2023-01-13 14:50:27 -0600

    Finished Balance from database

--- src/App.js
+++ src/App.js
@@ -4,12 +4,60 @@ import IncomeExpenseCard from "./components/IcomeExpenseCard";
 import History from "./components/History";
 import AddTransaction from "./components/AddTransaction";
 
+import { useState, useEffect } from 'react';
+
 
 function App() {
+    const [transactions, setTransactions] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const transactionsFromServer = await fetchTransactions();
+
+            setTransactions(transactionsFromServer);
+        }
+
+        getData();
+
+    }, [])
+
+    const fetchTransactions = async () => {
+        const res = await fetch('http://localhost:5000/transactions');
+
+        const data = await res.json();
+
+        return data;
+    }
+
+    const getIcomeExpense = () => {
+        // const transactionsFromServer = a
+
+
+        const expense = transactions.filter((transaction) => {
+            return transaction.type === "EXPENSE";
+        })
+            .reduce((total, transaction) => {
+                return total + transaction.amount
+            }, 0)
+
+        const income = transactions.filter((transaction) => {
+            return transaction.type === "INCOME";
+        })
+            .reduce((total, transaction) => {
+                return total + transaction.amount
+            }, 0)
+
+        return {income, expense}
+
+    }
+
+    const balance = getIcomeExpense().income - getIcomeExpense().expense;
+
+
     return (
         <div className="container">
             <Header />
-            <BalanceView />
+            <BalanceView balance={balance} />
             <IncomeExpenseCard />
 
             <History />
--- src/components/BalanceView.js
+++ src/components/BalanceView.js
@@ -1,9 +1,9 @@
 
-const BalanceView = () => {
+const BalanceView = ({balance}) => {
     return (
         <div className="balance-view">
             <h4>Your Balance</h4>
-            <h1>{`$ ${500}`}</h1>
+            <h1>{`$ ${balance}`}</h1>
         </div>
     )
 }
